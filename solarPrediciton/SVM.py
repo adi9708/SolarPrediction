8b67a4df3f647602061d6f418f0214ef08cb9742
adi9708 <adipatni97@gmail.com>
2023-06-16 16:57:01 -0700

    Update

---
+++ solarPrediciton/SVM.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+%matplotlib inline
+bankdata = pd.read_csv("D:/Datasets/bill_authentication.csv")
+bankdata.shape
+bankdata.head()
+X = bankdata.drop('Class', axis=1)
+y = bankdata['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+
+svclassifier = SVC(kernel='poly', degree=8)
+svclassifier.fit(X_train, y_train)
+y_pred = svclassifier.predict(X_test)
+
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
---
+++ solarPrediciton/venv/Include/function/function.js
@@ -0,0 +1,167 @@
+
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(document).ready(function(){
+	
+	
+	$("#chat").hide();
+	
+	
+});
+
+
+function send()
+
+{
+ insertMessage();
+
+
+
+}
+
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    alert("m "+m);
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    alert("lllllllll");
+  }
+}
+
+function insertMessage() {
+
+  msg = $('.message-input').val();
+  alert(msg);
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.messages-content')).addClass('new');
+  setDate();
+  alert("pooooo");
+
+
+  fakeMsg();
+  
+  //alert(msg);
+  
+//  $.ajax({
+//
+//	  url:"AdminServlet",
+//	  type:"POST",
+//	  data:{action:"fetchquestion", msg:msg},
+//	  success:function(data)
+//	  {
+//
+//
+//		  if(data=="not")
+//			  {
+//			  alert("Not valid question!!");
+//			  return false;
+//			  }
+//		 fakeMessage(data);
+//	  }
+//
+//
+//  })
+  
+  
+  $('.message-input').val(null);
+  updateScrollbar();
+ 
+}
+
+
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+
+
+var fake=[
+'hiiii',
+'hello'
+
+]
+
+
+function fakeMessage() {
+
+//  if ($('.message-input').val() != '') {
+//    return false;
+//  }
+//
+//  alert("heyyyy");
+//  $('<div class="message loading new"><figure class="avatar"><img src="img/user.png" /></figure><span></span></div>').appendTo($('.messages-content'));
+//  updateScrollbar();
+//
+//  setTimeout(function() {
+//    $('.message.loading').remove();
+//    $('<div class="message new"><figure class="avatar"><img src="img/user.png"  /></figure>'+ fake[i] +'</div>').appendTo($('.messages-content')).addClass('new');
+//    setDate();
+//    updateScrollbar();
+//
+//  }, 1000 + (Math.random() * 20) * 100);
+
+}
+
+function fakeMsg(){
+alert("in function")
+//      con=pymysql(host="localhost",user="root",password="",database="solarprediction")
+//      cur = con.cursor()
+//      sql="select * from solarprediction"
+//      cur.execute(sql)
+//      rows=cur.fetchall()
+//       for row in rows:
+//        print(row)
+//        print(row[1])
+jQuery.support.cors = true;
+$.ajax({
+type:"POST",
+url:'/fetchAnswers',
+success:function(data)
+{
+alert(data)
+}
+});
+
+      $('<div class="message new"><figure class="avatar"><img src="img/user.png"  /></figure>'+ data +'</div>').appendTo($('.messages-content')).addClass('new');
+
+}
+
+function openForm()
+{
+$("#chat").show();
+	
+}
+
+$('#closechat').click(function(){
+	
+	$("#chat").hide();
+
+});
+
+
+
+
+
+
+	
