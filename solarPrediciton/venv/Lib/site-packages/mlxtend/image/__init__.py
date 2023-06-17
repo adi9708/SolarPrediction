20dd7d0d3505f95f0b7b1780c9e6aa9125f35f0b
adi9708 <118132456+adi9708@users.noreply.github.com>
2023-06-16 17:04:39 -0700

    Delete solarPrediciton/venv directory

--- solarPrediciton/venv/Lib/site-packages/mlxtend/image/__init__.py
+++
@@ -1,9 +0,0 @@
-# Sebastian Raschka 2014-2018
-# mlxtend Machine Learning Library Extensions
-# Author: Sebastian Raschka <sebastianraschka.com>
-#
-# License: BSD 3 clause
-
-from .extract_face_landmarks import extract_face_landmarks
-
-__all__ = ["extract_face_landmarks"]
